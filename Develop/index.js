f9b67879117bf6a235d91b71f959714f55d06ec6
Artemis <wzyzhouyi@gmail.com>
2020-09-30 21:05:02 -0700

    linked generateMarkdown.js, added inquirer prompt and logged answers

--- Develop/index.js
+++ Develop/index.js
@@ -1,3 +1,7 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const data = require("./utils/generateMarkdown.js");
+
 // array of questions for user
 const questions = [
     "What is your GitHub username?",
@@ -11,6 +15,42 @@ const questions = [
     "What does the user need to know about contributing to the repo?"
 ];
 
+// test questions
+// const questions = [
+//     "What is your GitHub username?",
+//     "What kind of license should your project have?"
+// ];
+
+// array of names 
+const nameArray = ["username", "email", "title", "description", "license", "dependencyCommand", "testCommand", "usage", "contribution"];
+
+// test names
+// const nameArray = ["username", "license"];
+
+// array of license choices for license question
+const licenseChoices = ["MIT", "Apache License 2.0", "BSD"];
+
+let promptObj = [];
+
+for (let i = 0; i < nameArray.length; i++) {
+    promptObj[i] = {
+        type: "input",
+        message: questions[i],
+        name: nameArray[i]
+    }
+    if (i === 4) {
+        promptObj[i].type = "list";
+        promptObj[i].choices = licenseChoices;
+    }
+}
+
+// console.log(promptObj);
+
+inquirer
+    .prompt(promptObj)
+    .then(function(answers) {
+        console.log(answers);
+    });
 // function to write README file
 function writeToFile(fileName, data) {
 }
